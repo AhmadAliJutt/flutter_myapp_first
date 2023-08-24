44995e18c2d8c21eaf40b2bec540467d76559da1
AhmadAliJutt <ahmadalijutt9865@gmail.com>
2023-08-24 13:47:12 +0500

    FOlders are Updated

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_myapp_first/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
